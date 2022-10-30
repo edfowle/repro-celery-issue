4a28ef4aa52ca21f0977b8787af6b2a64ea03fad
Ed Fowle <ed.fowle@gmail.com>
2022-10-30 10:11:56 +0000

    added producer logging

--- src/producer/main.py
+++ src/producer/main.py
@@ -1,5 +1,8 @@
 import logging
+import os
 from shared.tasks import add
+os.makedirs('/var/log/repro_celeryissue', exist_ok=true)
+logging.basicConfig(filename="/var/log/repro_celeryissue/producer.log", level=logging.INFO)
 
 logging.info('calling task')
 result = add.delay(66,4)
